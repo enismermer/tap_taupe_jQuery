6bf846f5eb68a599f6aa88afdf55c0f5ec17cb14
Enis <enis.mermer.simplon@gmail.com>
2023-07-03 15:41:14 +0200

    ok

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 $(document).ready(function() 
 {
     let score = 1;
+    let random;
+    function timeActive() {
+        clearInterval(random)
+    }
     // en cliquant sur "commencer", une image taupe apparait
     $("#btCommencer").click(function() {
         // l'image taupe apparait aléatoirement
-        setInterval(function () {
+        random = setInterval(function () {
             $(".taupe").fadeIn()
             var random = Math.floor(Math.random() * jQuery('.taupe').length);
             jQuery('.taupe').hide().eq(random).show();
         }, 900)
 
+            setTimeout(timeActive, 30000);
+            chrono--;
         // une fois le jeu commencé, quand on clique sur l'image taupe
         $(".taupe").click(function() {
             // l'image taupe disparait
@@ -18,4 +24,10 @@ $(document).ready(function()
             $("#score").html("Score : " + score++ + " pts")
         })
     })
+
+
+        // chrono 30 secondes
+
+        let chrono = 30;
+        
 })
